52dfa51fef210363374758c14935364806965615
yuhcee <egbouchenna001@gmail.com>
2025-09-29 04:43:37 +0100

    fix(modal): correct modal root placement for Vite

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <div id="modal-root" />
     </QueryClientProvider>
   </React.StrictMode>
 );
