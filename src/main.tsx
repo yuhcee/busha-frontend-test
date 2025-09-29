f1d2902754d9156badcd722d23c4a6a6734a7058
yuhcee <egbouchenna001@gmail.com>
2025-09-29 04:07:19 +0100

    refactor: wrap app in QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import "./index.scss";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <div id="modal-root" />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <div id="modal-root" />
+    </QueryClientProvider>
   </React.StrictMode>
 );
